2a3cfeac78815e69a5750f42cae2d28e5ffc5a40
Yaroslav Smirnov <yaroslav.smirnov@inbox.ru>
2016-03-08 09:13:42 +0000

    edited commands list

--- app/controllers/commands/help.js
+++ app/controllers/commands/help.js
@@ -1,10 +1,10 @@
 module.exports = function(name) {
     var response = "Напоминаю, " + name + ", обращайся ко мне так: \n\n"
                         + "\/start - начать диалог \n"
-                        + "\/motivateme - получить мотивацию для бани \n"
+                        + "\/motivateme - получить мотивацию \n"
                         + "\/trollme - потроллить себя за несоблюдение традиции \n"
-                        + "\/trolluser - потроллить другого юзера\n"
-                        + "\/requestfeature - запросить новый пар \n"
+                        + "\/trolluser - потроллить другого\n"
+                        + "\/requestfeature - запросить фичу\n"
                         + "\/reportbug - сообщить о баге \n"
                         + "\/cancel - отменить команду\n"
                         + "\/help - попросить помощь\n\n";
