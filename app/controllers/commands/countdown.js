69bd180c9eec374040f78a63d02d1010da3a557b
Yaroslav Smirnov <yaroslav.smirnov@inbox.ru>
2016-08-07 08:32:56 +0000

    feat: calculate time of banya before and in active phase

--- app/controllers/commands/countdown.js
+++ app/controllers/commands/countdown.js
@@ -1,47 +1,111 @@
+require('datejs');
+
+function describe(tMinus, descriptor) {
+  
+  var options = [];
+  
+  switch(descriptor) {
+    case "дней":
+      options = ["день", "дня", "дней"];
+      break;
+    case "часов":
+      options = ["час", "часа", "часов"];
+      break;
+    case "минут":
+      options = ["минута", "минуты", "минут"];
+      break;
+    case "секунд":
+      options = ["секунда", "секунды", "секунд"];
+      break;
+  }    
+  
+  
+  if (tMinus >= 11 && tMinus <= 20) {
+    return options[2];
+  } else if (tMinus % 10 == 1) {
+    return options[0];
+  } else if (tMinus % 10 > 1 && tMinus % 10 < 5) {
+    return options[1];
+  } else {
+    return options[2];
+  }
+  
+}
+
+
 module.exports = function() {
-    var now = new Date();
-    // console.log(now.toISOString());
-    var offset = now.getTimezoneOffset();
-    var next = new Date(now).setSeconds(0);
-    next = new Date(next).setDate(now.getDate() + 7 - now.getDay() + 3);
-    next = new Date(next).setMinutes(0);
-    next = new Date(next).setHours(19 - 180 / 60);
-    // console.log(new Date(next).toISOString());
+  
+  // Everything here is based on UTC time; need to change if daylight saving time is introduced again in Russia 
+  
+  var now = new Date();
+  // now = new Date(2016, 7, 9, 23, 32, 41); // testing data
+  console.log("-- current data and time in UTC: " + new Date(now).toISOString());
+  
+  var days = (1000 * 60 * 60 * 24);
+  var hours = (60 * 60 * 1000);
+  var minutes = (60 * 1000);
+  var seconds = 1000;
+  
+  if (now.getDay() == 3 && now.getHours() >= 16 && now.getHours() <= 19) {
+    var start = new Date(now).setHours(16);
+    start = new Date(start).setMinutes(0);
+    start = new Date(start).setSeconds(0);
+    var diff = Math.abs(start - new Date(now));
+    var hoursRunning = Math.floor(diff / hours);
+    var minutesRunning = Math.floor(((diff - hoursRunning * hours) % hours) / minutes);
+    var secondsRunning = Math.floor(((diff - hoursRunning * hours - minutesRunning * minutes) % minutes) / seconds);
     
-    var diff = next - Date.now() - offset * 60 * 1000;
-    
-    var days = (1000 * 60 * 60 * 24);
-    var hours = (60 * 60 * 1000);
-    var minutes = (60 * 1000);
-    var seconds = 1000;
-    
-    var daysLeft = Math.floor(diff / days);
-    var hoursLeft = Math.floor(((diff - daysLeft * days) % days) / hours);
-    var minutesLeft = Math.floor(((diff - daysLeft * days - hoursLeft * hours) % hours) / minutes);
-    var secondsLeft = Math.floor(((diff - daysLeft * days - hoursLeft * hours - minutesLeft * minutes) % minutes) / seconds);
-    
-    var tMinus = [daysLeft, hoursLeft, minutesLeft, secondsLeft];
-    
-    tMinus.forEach(function(el) {
-      return (el < 10) ? "0" + el : el;
+    var running = [hoursRunning, minutesRunning, secondsRunning];
+    var descriptors = running.map(function(el, idx) {
+      var temp = describe(el, ["часов", "минут", "секунд"][idx]);
+      var out;
+      switch (temp) {
+        case "минута":
+          out = "минуту";
+          break;
+        case "секунда":
+          out = "секунду";
+          break;
+        default:
+          out = temp;
+          break;
+      }
+      return out;
     });
     
-    var tMinusDescriptors = ["дней", "часов", "минут", "секунд"];
-    
-    tMinusDescriptors[0] = (tMinus[0] % 10 == 1) ? "день" : "дней";
-    tMinusDescriptors[0] = (tMinus[0] > 1 && tMinus[0] <= 4 && tMinus[0] % 10 != 1) ? "дня" : tMinusDescriptors[0];
-    tMinusDescriptors[1] = (tMinus[1] % 10 == 1) ? "час" : "часов";
-    tMinusDescriptors[1] = (tMinus[1] % 10 > 1 && tMinus[1] % 10 <= 4 && tMinus[1] % 10 != 1) ? "часа" : tMinusDescriptors[1];
-    tMinusDescriptors[2] = (tMinus[2] % 10 == 1) ? "минута" : "минут";
-    tMinusDescriptors[2] = (tMinus[2] % 10 > 1 && tMinus[2] % 10 <= 4 && tMinus[2] % 10 != 1) ? "минуты" : tMinusDescriptors[2];
-    tMinusDescriptors[3] = (tMinus[3] % 10 == 1) ? "секунда" : "секунд";
-    tMinusDescriptors[3] = (tMinus[3] % 10 > 1 && tMinus[3] % 10 <= 4 && tMinus[3] % 10 != 1) ? "секунды" : tMinusDescriptors[3];
-    
-    
-    // console.log("T minus " + tMinus[0] + ":" + tMinus[1] + ":" + tMinus[2] + ":" + tMinus[3] + " and counting");
-    return ("До бани осталось " +
-                tMinus[0] + " " + tMinusDescriptors[0] + " " +
-                tMinus[1] + " " + tMinusDescriptors[1] + " " +
-                tMinus[2] + " " + tMinusDescriptors[2] + " " +
-                tMinus[3] + " " + tMinusDescriptors[3] + ".");
+    return ("Баня в активной фазе " +
+              running[0] + " " + descriptors[0] + " " +
+              running[1] + " " + descriptors[1] + " " +
+              running[2] + " " + descriptors[2] + ".");
+  }
+
+  var next = new Date(now);
+  
+  if (next.getDay() != 3 || (next.getDay() == 3 && next.getHours() >= 19)) {
+    next = next.next().wednesday();
+  }
+
+  next = new Date(next).setSeconds(0);
+  next = new Date(next).setMinutes(0);
+  next = new Date(next).setHours(16); // UTC time is used; need to change if daylight saving time is introduced 
+  
+  var diff = next - new Date(now); // UTC time is used; change if daylight saving time is introduced
+  
+  var daysLeft = Math.floor(diff / days);
+  var hoursLeft = Math.floor(((diff - daysLeft * days) % days) / hours);
+  var minutesLeft = Math.floor(((diff - daysLeft * days - hoursLeft * hours) % hours) / minutes);
+  var secondsLeft = Math.floor(((diff - daysLeft * days - hoursLeft * hours - minutesLeft * minutes) % minutes) / seconds);
+  
+  var tMinus = [daysLeft, hoursLeft, minutesLeft, secondsLeft];
+  var tMinusDescriptors = tMinus.map(function(el, idx) {
+    return describe(el, ["дней", "часов", "минут", "секунд"][idx]);
+  });
+  
+  
+  // console.log("T minus " + tMinus[0] + ":" + tMinus[1] + ":" + tMinus[2] + ":" + tMinus[3] + " and counting");
+  return ("До бани осталось " +
+              tMinus[0] + " " + tMinusDescriptors[0] + " " +
+              tMinus[1] + " " + tMinusDescriptors[1] + " " +
+              tMinus[2] + " " + tMinusDescriptors[2] + " " +
+              tMinus[3] + " " + tMinusDescriptors[3] + ".");
 }
